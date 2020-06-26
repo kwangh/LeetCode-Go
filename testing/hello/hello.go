5f56207d700249df66bd2a260172a065e88c695f
Kwang Hun <mm9139@naver.com>
2020-06-26 16:45:22 +0900

    go test tutorial follow up

---
+++ testing/hello/hello.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"example.com/user/hello/morestrings"
+)
+
+func main() {
+	fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
+}
---
+++ testing/hello/morestrings/example_test.go
@@ -0,0 +1,10 @@
+package morestrings
+
+import (
+	"fmt"
+)
+
+func ExampleReverseRunes() {
+	fmt.Println(ReverseRunes("hello"))
+	// Output: olleh
+}
