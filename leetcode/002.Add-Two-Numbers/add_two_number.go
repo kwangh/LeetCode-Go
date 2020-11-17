4703d1357a93569c162a5e79ec021cb474ff213a
Kwang Hun <mm9139@naver.com>
2020-11-17 15:56:01 +0900

    fix nil pointer dereference

--- leetcode/002.Add-Two-Numbers/add_two_number.go
+++ leetcode/002.Add-Two-Numbers/add_two_number.go
@@ -9,23 +9,25 @@ type ListNode struct {
 // AddTwoNumbers function adds two list numbers
 func AddTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 	res := &ListNode{}
-	cur := res
-
-	l1 = &ListNode{Next: l1}
-	l2 = &ListNode{Next: l2}
-
-	for *l1.Next != (ListNode{}) {
-		l1 = l1.Next
-		l2 = l2.Next
-
-		sum := cur.Val + l1.Val + l2.Val
-		mod := sum % 10
-
-		cur.Val = mod
-		cur.Next = &ListNode{}
+	n1, n2, carry, sum, cur := 0, 0, 0, 0, res
+	for l1 != nil || l2 != nil || carry != 0 {
+		if l1 != nil {
+			n1 = l1.Val
+			l1 = l1.Next
+		} else {
+			n1 = 0
+		}
+		if l2 != nil {
+			n2 = l2.Val
+			l2 = l2.Next
+		} else {
+			n2 = 0
+		}
+		sum = n1 + n2 + carry
+		cur.Next = &ListNode{Val: sum % 10}
 		cur = cur.Next
-		cur.Val = (sum - mod) / 10
+		carry = sum / 10
 	}
 
-	return res
+	return res.Next
 }
--- leetcode/002.Add-Two-Numbers/add_two_number_test.go
+++ leetcode/002.Add-Two-Numbers/add_two_number_test.go
@@ -9,23 +9,19 @@ func TestAddTwoNumbers(t *testing.T) {
 		in1, in2, want *ListNode
 	}{
 		{
-			&ListNode{Val: 2, Next: &ListNode{Val: 4, Next: &ListNode{Val: 3, Next: &ListNode{}}}},
-			&ListNode{Val: 5, Next: &ListNode{Val: 6, Next: &ListNode{Val: 4, Next: &ListNode{}}}},
-			&ListNode{Val: 7, Next: &ListNode{Val: 0, Next: &ListNode{Val: 8, Next: &ListNode{}}}},
+			&ListNode{Val: 2, Next: &ListNode{Val: 4, Next: &ListNode{Val: 3}}},
+			&ListNode{Val: 5, Next: &ListNode{Val: 6, Next: &ListNode{Val: 4}}},
+			&ListNode{Val: 7, Next: &ListNode{Val: 0, Next: &ListNode{Val: 8}}},
 		},
 	}
 	for _, c := range cases {
 		got := AddTwoNumbers(c.in1, c.in2)
-		for *c.want.Next != (ListNode{}) {
+		for c.want != nil {
 			if got.Val != c.want.Val {
 				t.Error()
 			}
 			c.want = c.want.Next
 			got = got.Next
-		}
-
-		if got.Val != c.want.Val {
-			t.Error()
 		}
 	}
 }
