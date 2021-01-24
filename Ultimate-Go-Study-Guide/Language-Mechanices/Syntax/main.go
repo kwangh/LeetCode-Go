2b92b5d7edf378d129875180bf70dc522a3bf1e8
Kwang Hun <mm9139@naver.com>
2021-01-24 22:54:22 +0900

    ultimate go study start

---
+++ Ultimate-Go-Study-Guide/Language-Mechanices/Syntax/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a int
+	var b string
+	var c float64
+	var d bool
+	fmt.Printf("var a int \t %T [%v]\n", a, a)
+	fmt.Printf("var b string \t %T [%v]\n", b, b)
+	fmt.Printf("var c float64 \t %T [%v]\n", c, c)
+	fmt.Printf("var d bool \t %T [%v]\n\n", d, d)
+
+	aa := 10
+	bb := "hello"
+	cc := 3.14159
+	dd := true
+
+	fmt.Printf("aa := 10 \t %T [%v]\n", aa, aa)
+	fmt.Printf("bb := \"hello\" \t %T [%v]\n", bb, bb)
+	fmt.Printf("cc := 3.14159 \t %T [%v]\n", cc, cc)
+	fmt.Printf("dd := true \t %T [%v]\n\n", dd, dd)
+}
