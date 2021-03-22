43b342a8e87f6238c2489df5b8d58526aff25810
Kwang Hun <mm9139@naver.com>
2021-03-22 23:00:22 +0900

    48. roate image version2

--- leetcode/048.Rotate-Image/rotate_image.go
+++ leetcode/048.Rotate-Image/rotate_image.go
@@ -1,6 +1,16 @@
 package leetcode
 
 func rotate(matrix [][]int) {
+	for row := 0; row < len(matrix)/2; row++ {
+		for col := row; col < len(matrix)-1-row; col++ {
+			matrix[row][col], matrix[col][len(matrix)-1-row] = matrix[col][len(matrix)-1-row], matrix[row][col]
+			matrix[row][col], matrix[len(matrix)-1-row][len(matrix)-1-col] = matrix[len(matrix)-1-row][len(matrix)-1-col], matrix[row][col]
+			matrix[row][col], matrix[len(matrix)-1-col][row] = matrix[len(matrix)-1-col][row], matrix[row][col]
+		}
+	}
+}
+
+func rotateV1(matrix [][]int) {
 	for i := 0; i < len(matrix)-1; i++ {
 		for j := i + 1; j < len(matrix[i]); j++ {
 			matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
