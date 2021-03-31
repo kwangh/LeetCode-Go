7cef0e2612256e1bf53efd34c5bf41b3091d30eb
Kwang Hun <mm9139@naver.com>
2021-03-31 16:55:51 +0900

    use less cpu version, less check

--- leetcode/079.Word-Search/word_search.go
+++ leetcode/079.Word-Search/word_search.go
@@ -1,22 +1,25 @@
 package leetcode
 
-func start(visited *[][]bool, board [][]byte, i, j int, word string) bool {
-	if word == "" {
-		return true
+func existNextChar(visited [][]bool, board [][]byte, i, j int, word string, c int) bool {
+	if c == len(word)-1 {
+		return board[i][j] == word[c]
 	}
 
-	if i >= 0 && i < len(board) && j >= 0 && j < len(board[0]) {
-		if !(*visited)[i][j] && word[0] == board[i][j] {
-			(*visited)[i][j] = true
-			if start(visited, board, i+1, j, word[1:]) ||
-				start(visited, board, i-1, j, word[1:]) ||
-				start(visited, board, i, j+1, word[1:]) ||
-				start(visited, board, i, j-1, word[1:]) {
-				return true
-			} else {
-				(*visited)[i][j] = false
-			}
+	if word[c] == board[i][j] {
+		visited[i][j] = true
+		if i+1 < len(board) && !visited[i+1][j] && existNextChar(visited, board, i+1, j, word, c+1) {
+			return true
 		}
+		if i-1 >= 0 && !visited[i-1][j] && existNextChar(visited, board, i-1, j, word, c+1) {
+			return true
+		}
+		if j+1 < len(board[0]) && !visited[i][j+1] && existNextChar(visited, board, i, j+1, word, c+1) {
+			return true
+		}
+		if j-1 >= 0 && !visited[i][j-1] && existNextChar(visited, board, i, j-1, word, c+1) {
+			return true
+		}
+		visited[i][j] = false
 	}
 
 	return false
@@ -27,17 +30,10 @@ func exist(board [][]byte, word string) bool {
 	for i := range visited {
 		visited[i] = make([]bool, len(board[0]))
 	}
-	for i := 0; i < len(board); i++ {
-		for j := 0; j < len(board[0]); j++ {
-			if board[i][j] == word[0] {
-				if start(&visited, board, i, j, word) {
-					return true
-				}
-				for k := 0; k < len(board); k++ {
-					for l := 0; l < len(board[0]); l++ {
-						visited[k][l] = false
-					}
-				}
+	for i := range board {
+		for j := range board[0] {
+			if existNextChar(visited, board, i, j, word, 0) {
+				return true
 			}
 		}
 	}
