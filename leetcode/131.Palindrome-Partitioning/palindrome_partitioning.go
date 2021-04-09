72f39f5684db15822f98c103eacd475c3092153a
Kwang Hun <mm9139@naver.com>
2021-04-09 16:48:11 +0900

    dp solution

--- leetcode/131.Palindrome-Partitioning/palindrome_partitioning.go
+++ leetcode/131.Palindrome-Partitioning/palindrome_partitioning.go
@@ -1,25 +1,15 @@
 package leetcode
 
-func palindrome(s string) bool {
-	l, r := 0, len(s)-1
-	for l < r {
-		if s[l] != s[r] {
-			return false
-		}
-		l++
-		r--
-	}
-	return true
-}
-
-func dfs(res *[][]string, cur []string, s string, start int) {
+func dfs(res *[][]string, cur []string, s string, start int, dp [][]bool) {
 	if start == len(s) {
 		*res = append(*res, append([]string{}, cur...))
+		return
 	}
 	for end := start; end < len(s); end++ {
-		if palindrome(s[start : end+1]) {
+		if s[start] == s[end] && (end-start <= 2 || dp[start+1][end-1]) {
+			dp[start][end] = true
 			cur = append(cur, s[start:end+1])
-			dfs(res, cur, s, end+1)
+			dfs(res, cur, s, end+1, dp)
 			cur = cur[:len(cur)-1]
 		}
 	}
@@ -27,6 +17,10 @@ func dfs(res *[][]string, cur []string, s string, start int) {
 
 func partition(s string) [][]string {
 	var res [][]string
-	dfs(&res, []string{}, s, 0)
+	dp := make([][]bool, len(s))
+	for i := range dp {
+		dp[i] = make([]bool, len(s))
+	}
+	dfs(&res, []string{}, s, 0, dp)
 	return res
 }
