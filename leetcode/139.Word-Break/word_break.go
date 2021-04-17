806f5789ef63a3b0023579620bc9038138c79ece
KwangHun Choi <mm9139@naver.com>
2021-04-18 03:21:40 +0900

    remove blank spaces

--- leetcode/139.Word-Break/word_break.go
+++ leetcode/139.Word-Break/word_break.go
@@ -5,10 +5,8 @@ func wordBreak(s string, wordDict []string) bool {
 	for _, v := range wordDict {
 		m[v] = struct{}{}
 	}
-
 	dp := make([]bool, len(s)+1)
 	dp[0] = true
-
 	for i := 1; i <= len(s); i++ {
 		for j := 0; j < i; j++ {
 			if dp[j] {
