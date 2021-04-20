cdb3d3cade265504ea1a05b2de6a1daf4b3c2f40
Kwang Hun <mm9139@naver.com>
2021-04-20 16:33:18 +0900

    less iteration, less memory usage

--- leetcode/021.Merge-Two-Sorted-Lists/merge_two_sorted_lists.go
+++ leetcode/021.Merge-Two-Sorted-Lists/merge_two_sorted_lists.go
@@ -11,25 +11,19 @@ func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
 	t := res
 	for l1 != nil && l2 != nil {
 		if l1.Val < l2.Val {
-			t.Next = &ListNode{Val: l1.Val}
+			t.Next = l1
 			t = t.Next
 			l1 = l1.Next
 		} else {
-			t.Next = &ListNode{Val: l2.Val}
+			t.Next = l2
 			t = t.Next
 			l2 = l2.Next
 		}
 	}
-	for l1 != nil {
-		t.Next = &ListNode{Val: l1.Val}
-		t = t.Next
-		l1=l1.Next
-	}
-
-	for l2 != nil {
-		t.Next = &ListNode{Val: l2.Val}
-		t = t.Next
-		l2=l2.Next
+	if l1 != nil {
+		t.Next = l1
+	} else {
+		t.Next = l2
 	}
 	return res.Next
 }
