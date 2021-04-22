f13fb588085da32572d29d978b53ed0ed0356942
Kwang Hun <mm9139@naver.com>
2021-04-22 13:29:04 +0900

    less add operation

--- leetcode/053.Maximum-Subarray/maximum_subarray.go
+++ leetcode/053.Maximum-Subarray/maximum_subarray.go
@@ -3,11 +3,12 @@ package leetcode
 func maxSubArray(nums []int) int {
 	max, pre := nums[0], nums[0]
 	for i := 1; i < len(nums); i++ {
-		if pre+nums[i] < nums[i] {
-			pre = nums[i]
+		if pre > 0 {
+			pre += nums[i]
 		} else {
-			pre = pre + nums[i]
+			pre = nums[i]
 		}
+
 		if pre > max {
 			max = pre
 		}
