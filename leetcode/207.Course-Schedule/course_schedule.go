218cf0ea0356b8f6e2c64f5e7740a542ff9a5392
Kwang Hun <mm9139@naver.com>
2021-04-30 17:16:04 +0900

    bfs solution included

--- leetcode/207.Course-Schedule/course_schedule.go
+++ leetcode/207.Course-Schedule/course_schedule.go
@@ -20,6 +20,42 @@ func isAcyclic(g map[int][]int, visited, cur map[int]struct{}, node int) bool {
 	return true
 }
 
+func canFinishBFS(numCourses int, prerequisites [][]int) bool {
+	graph := make(map[int][]int)
+	for _, p := range prerequisites {
+		graph[p[1]] = append(graph[p[1]], p[0])
+	}
+
+	degrees := make([]int, numCourses)
+	for _, g := range graph {
+		for _, v := range g {
+			degrees[v]++
+		}
+	}
+
+	var nodes []int
+	for i := 0; i < numCourses; i++ {
+		if degrees[i] == 0 {
+			nodes = append(nodes, i)
+		}
+	}
+
+	edges := len(prerequisites)
+	for len(nodes) != 0 {
+		cur := nodes[0]
+		nodes = nodes[1:]
+		for _, node := range graph[cur] {
+			edges--
+			degrees[node]--
+			if degrees[node] == 0 {
+				nodes = append(nodes, node)
+			}
+		}
+	}
+
+	return edges == 0
+}
+
 func canFinish(numCourses int, prerequisites [][]int) bool {
 	graph := make(map[int][]int)
 	for _, p := range prerequisites {
