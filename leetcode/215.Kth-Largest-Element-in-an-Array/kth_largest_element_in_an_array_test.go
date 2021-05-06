3bd02bb04418a4dd74173a3cd8fa91fd5bb3d49f
Kwang Hun <mm9139@naver.com>
2021-05-06 23:02:10 +0900

    215. Kth Largest Element in an Array

---
+++ leetcode/215.Kth-Largest-Element-in-an-Array/kth_largest_element_in_an_array_test.go
@@ -0,0 +1,20 @@
+package leetcode
+
+import "testing"
+
+func TestFindKthLargest(t *testing.T) {
+	cases := []struct {
+		nums []int
+		k    int
+		want int
+	}{
+		{[]int{3, 2, 1, 5, 6, 4}, 2, 5},
+		{[]int{3, 2, 3, 1, 2, 4, 5, 5, 6}, 4, 4},
+	}
+	for _, c := range cases {
+		got := findKthLargest(c.nums, c.k)
+		if c.want != got {
+			t.Errorf("want:%d instead got:%d", c.want, got)
+		}
+	}
+}
