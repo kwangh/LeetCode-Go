83790a0f36d44890623f7f149a3ca7b3af8f0be9
Kwang Hun <mm9139@naver.com>
2021-05-07 16:13:13 +0900

    optimized version

--- leetcode/227.Basic-Calculator2/basic_calculator2.go
+++ leetcode/227.Basic-Calculator2/basic_calculator2.go
@@ -1,8 +1,7 @@
 package leetcode
 
 func calculate(s string) int {
-	var stack []int
-	cur := 0
+	cur, last, res := 0, 0, 0
 	op := byte('+')
 	for i := 0; i < len(s); i++ {
 		c := s[i]
@@ -11,25 +10,20 @@ func calculate(s string) int {
 		}
 		if !(c >= '0' && c <= '9') && c != ' ' || i == len(s)-1 {
 			if op == '+' {
-				stack = append(stack, cur)
+				res += last
+				last = cur
 			} else if op == '-' {
-				stack = append(stack, -cur)
+				res += last
+				last = -cur
 			} else if op == '*' {
-				pop := stack[len(stack)-1]
-				stack = stack[:len(stack)-1]
-				stack = append(stack, pop*cur)
+				last *= cur
 			} else if op == '/' {
-				pop := stack[len(stack)-1]
-				stack = stack[:len(stack)-1]
-				stack = append(stack, pop/cur)
+				last /= cur
 			}
 			op = c
 			cur = 0
 		}
 	}
-	var res int
-	for _, v := range stack {
-		res += v
-	}
+	res += last
 	return res
 }
