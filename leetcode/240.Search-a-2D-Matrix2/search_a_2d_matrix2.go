34e0fa54d0840e9fac6387cf24e884d3d8e47364
Kwang Hun <mm9139@naver.com>
2021-05-12 15:32:31 +0900

    O(m+n) solution

--- leetcode/240.Search-a-2D-Matrix2/search_a_2d_matrix2.go
+++ leetcode/240.Search-a-2D-Matrix2/search_a_2d_matrix2.go
@@ -1,29 +1,15 @@
 package leetcode
 
-func search(matrix [][]int, v [][]bool, target, row, col int) bool {
-	if row == len(matrix) || col == len(matrix[0]) {
-		return false
-	}
-	if v[row][col] {
-		return false
-	}
-	v[row][col] = true
-
-	if matrix[row][col] == target {
-		return true
-	}
-
-	if matrix[row][col] < target {
-		return search(matrix, v, target, row+1, col) || search(matrix, v, target, row, col+1)
-	}
-
-	return false
-}
-
 func searchMatrix(matrix [][]int, target int) bool {
-	v := make([][]bool, len(matrix))
-	for i := range v {
-		v[i] = make([]bool, len(matrix[0]))
+	r, c := 0, len(matrix[0])-1
+	for r < len(matrix) && c >= 0 {
+		if matrix[r][c] == target {
+			return true
+		} else if matrix[r][c] < target {
+			r++
+		} else {
+			c--
+		}
 	}
-	return search(matrix, v, target, 0, 0)
+	return false
 }
