45710d7be2aabcd90ff09d0084c6e4acafe167c6
Kwang Hun <mm9139@naver.com>
2021-05-27 14:57:02 +0900

    108. Convert Sorted Array to Binary Search Tree

--- leetcode/108.Convert-Sorted-Array-to-Binary-Search-Tree/convert_sorted_array_to_binary_search_tree.go
+++ leetcode/108.Convert-Sorted-Array-to-Binary-Search-Tree/convert_sorted_array_to_binary_search_tree.go
@@ -7,5 +7,13 @@ type TreeNode struct {
 }
 
 func sortedArrayToBST(nums []int) *TreeNode {
-	
+	if len(nums) == 0 {
+		return nil
+	}
+	m := len(nums) / 2
+	return &TreeNode{
+		Val:   nums[m],
+		Left:  sortedArrayToBST(nums[:m]),
+		Right: sortedArrayToBST(nums[m+1:]),
+	}
 }
