a859643b6c2b251ab2ca2ba9953cade57a514853
Kwang Hun <mm9139@naver.com>
2021-05-31 22:34:50 +0900

    155. tmp

---
+++ leetcode/155.Min-Stack/min_stack.go
@@ -0,0 +1,24 @@
+package leetcode
+
+type MinStack struct {
+}
+
+func Constructor() MinStack {
+
+}
+
+func (this *MinStack) Push(val int) {
+
+}
+
+func (this *MinStack) Pop() {
+
+}
+
+func (this *MinStack) Top() int {
+
+}
+
+func (this *MinStack) GetMin() int {
+
+}
