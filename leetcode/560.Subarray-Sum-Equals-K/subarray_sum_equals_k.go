283d4ad92baf2f75f916587ac7da37085f72075c
Kwang Hun <mm9139@naver.com>
2021-06-02 15:37:42 +0900

    560. Subarray Sum Equals K

--- leetcode/560.Subarray-Sum-Equals-K/subarray_sum_equals_k.go
+++ leetcode/560.Subarray-Sum-Equals-K/subarray_sum_equals_k.go
@@ -1,5 +1,13 @@
 package leetcode
 
 func subarraySum(nums []int, k int) int {
-	return 0
+	var res, sum int
+	m := make(map[int]int, len(nums)+1)
+	m[0] = 1
+	for _, num := range nums {
+		sum += num
+		res += m[sum-k]
+		m[sum]++
+	}
+	return res
 }
--- leetcode/560.Subarray-Sum-Equals-K/subarray_sum_equals_k_test.go
+++ leetcode/560.Subarray-Sum-Equals-K/subarray_sum_equals_k_test.go
@@ -10,4 +10,5 @@ func TestSubarraySum(t *testing.T) {
 	assert := assert.New(t)
 	assert.Equal(2, subarraySum([]int{1, 1, 1}, 2), "subarraySum([1,1,1],2) should be 2")
 	assert.Equal(2, subarraySum([]int{1, 2, 3}, 3), "subarraySum([1,2,3],3) should be 2")
+	assert.Equal(1, subarraySum([]int{1}, 1), "subarraySum([1],1) should be 1")
 }
