1bae3de700e8df940d4ecfb4a868d3e0db2ee314
Kwang Hun <mm9139@naver.com>
2021-06-04 00:36:02 +0900

    300. Longest Increasing Subsequence

--- leetcode/300.Longest-Increasing-Subsequence/longest_increasing_subsequence.go
+++ leetcode/300.Longest-Increasing-Subsequence/longest_increasing_subsequence.go
@@ -1,5 +1,23 @@
 package leetcode
 
 func lengthOfLIS(nums []int) int {
+	dp := make([]int, len(nums))
+	len := 0
+	for _, v := range nums {
+		i, j := 0, len
+		for i < j {
+			m := (i + j) / 2
+			if dp[m] < v {
+				i = m + 1
+			} else {
+				j = m
+			}
+		}
+		dp[i] = v
+		if i == len {
+			len++
+		}
+	}
 
+	return len
 }
--- leetcode/300.Longest-Increasing-Subsequence/longest_increasing_subsequence_test.go
+++ leetcode/300.Longest-Increasing-Subsequence/longest_increasing_subsequence_test.go
@@ -1,7 +1,15 @@
 package leetcode
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestLengthOfLIS(t *testing.T) {
-	
+	a := assert.New(t)
+	a.Equal(4, lengthOfLIS([]int{10, 9, 2, 5, 3, 7, 101, 18}), "lengthOfLIS([]int{10,9,2,5,3,7,101,18}) should be 4")
+	a.Equal(4, lengthOfLIS([]int{0, 1, 0, 3, 2, 3}), "lengthOfLIS([]int{0,1,0,3,2,3}) should be 4")
+	a.Equal(1, lengthOfLIS([]int{7, 7, 7, 7, 7, 7, 7}), "lengthOfLIS([]int{7,7,7,7,7,7,7}) should be 1")
+	a.Equal(6, lengthOfLIS([]int{1, 3, 6, 7, 9, 4, 10, 5, 6}), "lengthOfLIS([]int{1,3,6,7,9,4,10,5,6}) should be 6")
 }
