857e1857820a796725a7a9705e3590e0a8b9b85a
KwangHun Choi <mm9139@naver.com>
2021-06-06 00:16:48 +0900

    118. Pascal's Triangle

--- leetcode/118.Pascal-Triangle/pascal_triangle.go
+++ leetcode/118.Pascal-Triangle/pascal_triangle.go
@@ -1,5 +1,15 @@
 package leetcode
 
 func generate(numRows int) [][]int {
-	return nil
+	res := make([][]int, numRows)
+	res[0] = []int{1}
+	for i := 1; i < numRows; i++ {
+		res[i] = make([]int, i+1)
+		res[i][0] = 1
+		res[i][i] = 1
+		for j := 1; j < i; j++ {
+			res[i][j] = res[i-1][j-1] + res[i-1][j]
+		}
+	}
+	return res
 }
