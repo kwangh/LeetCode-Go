4c21ae46066226b8fbe10943f761e17ab4d80359
KwangHun Choi <mm9139@naver.com>
2021-07-18 00:26:46 +0900

    617. Merge Two Binary Trees

--- leetcode/617.Merge-Two-Binary-Trees/merge_two_binary_trees.go
+++ leetcode/617.Merge-Two-Binary-Trees/merge_two_binary_trees.go
@@ -7,18 +7,13 @@ type TreeNode struct {
 }
 
 func mergeTrees(root1 *TreeNode, root2 *TreeNode) *TreeNode {
-	if root1 == nil && root2 == nil {
-		return nil
-	}
-
 	if root1 == nil {
-		root1 = &TreeNode{}
+		return root2
 	}
-	if root2 != nil {
+	if root1 != nil && root2 != nil {
 		root1.Val += root2.Val
 		root1.Left = mergeTrees(root1.Left, root2.Left)
 		root1.Right = mergeTrees(root1.Right, root2.Right)
 	}
-
 	return root1
 }
