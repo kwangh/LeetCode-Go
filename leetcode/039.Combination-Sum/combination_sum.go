4351f87e18e4606c9214fa57ebb72fcfa939c506
KwangHun Choi <mm9139@naver.com>
2021-09-15 02:02:36 +0900

    without sorting candidates

--- leetcode/039.Combination-Sum/combination_sum.go
+++ leetcode/039.Combination-Sum/combination_sum.go
@@ -1,25 +1,20 @@
 package combinationsum
 
-import (
-	"sort"
-)
-
-func recursiveCombinations(candidates []int, target int, res *[][]int, cur []int) {
+func recursiveCombinations(candidates []int, target, index int, res *[][]int, cur []int) {
 	if target == 0 {
 		*res = append(*res, append([]int{}, cur...))
 		return
 	}
 
-	for i, v := range candidates {
-		if v <= target {
-			recursiveCombinations(candidates[i:], target-v, res, append(cur, v))
+	for i := index; i < len(candidates); i++ {
+		if candidates[i] <= target {
+			recursiveCombinations(candidates, target-candidates[i], i, res, append(cur, candidates[i]))
 		}
 	}
 }
 
 func combinationSum(candidates []int, target int) [][]int {
 	res := [][]int{}
-	sort.Sort(sort.Reverse(sort.IntSlice(candidates)))
-	recursiveCombinations(candidates, target, &res, nil)
+	recursiveCombinations(candidates, target, 0, &res, nil)
 	return res
 }
