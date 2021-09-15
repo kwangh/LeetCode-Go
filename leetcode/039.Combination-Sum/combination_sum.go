03a5894890f8034e365bb6288a2fcb409245b288
KwangHun Choi <mm9139@naver.com>
2021-09-15 16:15:38 +0900

    39. Combination Sum final

--- leetcode/039.Combination-Sum/combination_sum.go
+++ leetcode/039.Combination-Sum/combination_sum.go
@@ -1,20 +1,20 @@
 package combinationsum
 
-func recursiveCombinations(candidates []int, target, index int, res *[][]int, cur []int) {
+func recursiveCombinations(candidates []int, target int, res *[][]int, cur []int) {
 	if target == 0 {
 		*res = append(*res, append([]int{}, cur...))
 		return
 	}
 
-	for i := index; i < len(candidates); i++ {
-		if candidates[i] <= target {
-			recursiveCombinations(candidates, target-candidates[i], i, res, append(cur, candidates[i]))
+	for i, v := range candidates {
+		if v <= target {
+			recursiveCombinations(candidates[i:], target-v, res, append(cur, candidates[i]))
 		}
 	}
 }
 
 func combinationSum(candidates []int, target int) [][]int {
 	res := [][]int{}
-	recursiveCombinations(candidates, target, 0, &res, nil)
+	recursiveCombinations(candidates, target, &res, nil)
 	return res
 }
