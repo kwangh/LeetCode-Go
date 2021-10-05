86c26cef7a0f30b52488dfc4d544f9169ea671e2
KwangHun Choi <mm9139@naver.com>
2021-10-06 00:09:26 +0900

    189. Rotate Array

--- leetcode/189.Rotate-Array/rotate_array.go
+++ leetcode/189.Rotate-Array/rotate_array.go
@@ -1,20 +1,17 @@
 package leetcode
 
-func reverse(nums []int, l, r int) {
-	for l < r {
-		nums[l], nums[r] = nums[r], nums[l]
-		l++
-		r--
-	}
-}
-
 func rotate(nums []int, k int) {
-	if len(nums) == 1 || len(nums) == k || k == 0 {
+	k = k % len(nums)
+	if k == 0 {
 		return
 	}
-
-	k = k % len(nums)
 	reverse(nums, 0, len(nums)-1)
 	reverse(nums, 0, k-1)
 	reverse(nums, k, len(nums)-1)
+}
+
+func reverse(nums []int, l, r int) {
+	for ; l < r; l, r = l+1, r-1 {
+		nums[l], nums[r] = nums[r], nums[l]
+	}
 }
