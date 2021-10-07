76097041629df3e2e53cf09660e9a1a4ebc09997
huni-c <huni.c@kakaocorp.com>
2021-10-07 23:44:34 +0900

    79. Word Search 100%

--- leetcode/079.Word-Search/word_search.go
+++ leetcode/079.Word-Search/word_search.go
@@ -1,38 +1,31 @@
 package leetcode
 
-func existNextChar(visited [][]bool, board [][]byte, i, j int, word string, c int) bool {
-	if c == len(word)-1 {
-		return board[i][j] == word[c]
+func dfs(board [][]byte, i, j int, word string) bool {
+	if len(word) == 1 {
+		return true
 	}
 
-	if word[c] == board[i][j] {
-		visited[i][j] = true
-		if i+1 < len(board) && !visited[i+1][j] && existNextChar(visited, board, i+1, j, word, c+1) {
-			return true
-		}
-		if i-1 >= 0 && !visited[i-1][j] && existNextChar(visited, board, i-1, j, word, c+1) {
-			return true
-		}
-		if j+1 < len(board[0]) && !visited[i][j+1] && existNextChar(visited, board, i, j+1, word, c+1) {
-			return true
-		}
-		if j-1 >= 0 && !visited[i][j-1] && existNextChar(visited, board, i, j-1, word, c+1) {
-			return true
-		}
-		visited[i][j] = false
+	board[i][j] = '0'
+	if i-1 >= 0 && board[i-1][j] == word[1] && dfs(board, i-1, j, word[1:]) {
+		return true
 	}
-
+	if j-1 >= 0 && board[i][j-1] == word[1] && dfs(board, i, j-1, word[1:]) {
+		return true
+	}
+	if i+1 < len(board) && board[i+1][j] == word[1] && dfs(board, i+1, j, word[1:]) {
+		return true
+	}
+	if j+1 < len(board[0]) && board[i][j+1] == word[1] && dfs(board, i, j+1, word[1:]) {
+		return true
+	}
+	board[i][j] = word[0]
 	return false
 }
 
 func exist(board [][]byte, word string) bool {
-	visited := make([][]bool, len(board))
-	for i := range visited {
-		visited[i] = make([]bool, len(board[0]))
-	}
 	for i := range board {
 		for j := range board[0] {
-			if existNextChar(visited, board, i, j, word, 0) {
+			if board[i][j] == word[0] && dfs(board, i, j, word) {
 				return true
 			}
 		}
