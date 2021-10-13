ce97f9adb36f6605abcad41f1907e85a7d42f4b5
KwangHun Choi <mm9139@naver.com>
2021-10-13 18:57:06 +0900

    21. Merge Two Sorted Lists

--- leetcode/021.Merge-Two-Sorted-Lists/merge_two_sorted_lists.go
+++ leetcode/021.Merge-Two-Sorted-Lists/merge_two_sorted_lists.go
@@ -7,23 +7,29 @@ type ListNode struct {
 }
 
 func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+	if l2 == nil {
+		return l1
+	}
+	if l1 == nil {
+		return l2
+	}
+
 	res := &ListNode{}
-	t := res
+	tmp := res
 	for l1 != nil && l2 != nil {
 		if l1.Val < l2.Val {
-			t.Next = l1
-			t = t.Next
+			tmp.Next = l1
 			l1 = l1.Next
 		} else {
-			t.Next = l2
-			t = t.Next
+			tmp.Next = l2
 			l2 = l2.Next
 		}
+		tmp = tmp.Next
 	}
-	if l1 != nil {
-		t.Next = l1
-	} else {
-		t.Next = l2
+	if l1 == nil {
+		tmp.Next = l2
+	} else if l2 == nil {
+		tmp.Next = l1
 	}
 	return res.Next
 }
