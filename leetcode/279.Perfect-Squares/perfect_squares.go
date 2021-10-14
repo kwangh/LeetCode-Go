2b14b4b56fb0ac0cb730a25b1b84fdd4a2f12aeb
KwangHun Choi <mm9139@naver.com>
2021-10-15 02:27:51 +0900

    279. Perfect Squares

--- leetcode/279.Perfect-Squares/perfect_squares.go
+++ leetcode/279.Perfect-Squares/perfect_squares.go
@@ -1,35 +1,19 @@
 package leetcode
 
-func min(a, b int) int {
+func numSquares(n int) int {
+	dp := make([]int, n+1)
+	for i := 1; i <= n; i++ {
+		dp[i] = i
+		for j := 1; j*j <= i; j++ {
+			dp[i] = Min(dp[i], dp[i-j*j]+1)
+		}
+	}
+	return dp[n]
+}
+
+func Min(a, b int) int {
 	if a < b {
 		return a
 	}
 	return b
-}
-
-var dp = [10001]int{}
-
-func numSquares(n int) int {
-	var dfs func(i int) int
-	dfs = func(i int) int {
-		if i == 0 {
-			return 0
-		}
-
-		if dp[i] > 0 {
-			return dp[i]
-		}
-
-		res := i
-		for j := 1; j*j <= i; j++ {
-			res = min(res, dfs(i-j*j))
-		}
-
-		res++
-		dp[i] = res
-
-		return res
-	}
-
-	return dfs(n)
 }
