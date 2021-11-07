8c60be134bd9716f344d1f0b178e593d57908bd0
KwangHun Choi <mm9139@naver.com>
2021-11-08 02:00:30 +0900

    34. Find First and Last Position of Element in Sorted Array

--- leetcode/034.Find-First-and-Last-Position-of-Element-in-Sorted-Array/search_range.go
+++ leetcode/034.Find-First-and-Last-Position-of-Element-in-Sorted-Array/search_range.go
@@ -1,25 +1,36 @@
 package leetcode
 
 func searchRange(nums []int, target int) []int {
+	res := []int{-1, -1}
+	if len(nums) == 0 {
+		return res
+	}
 	l, r := 0, len(nums)-1
-	for l <= r {
-		m := l + (r-l)/2
-		if target < nums[m] {
-			r = m - 1
-		} else if target > nums[m] {
+	for l < r {
+		m := (l + r) / 2
+		if nums[m] < target {
 			l = m + 1
+		} else if nums[m] > target {
+			r = m - 1
 		} else {
-			l, r = m, m
-			for l >= 0 && nums[l] == target {
-				l--
-			}
-			for r < len(nums) && nums[r] == target {
-				r++
-			}
-
-			return []int{l + 1, r - 1}
+			r = m
 		}
 	}
-
-	return []int{-1, -1}
+	if nums[l] != target {
+		return res
+	}
+	res[0] = l
+	r = len(nums) - 1
+	for l < r {
+		m := (l+r)/2 + 1
+		if nums[m] > target {
+			r = m - 1
+		} else if nums[m] < target {
+			l = m + 1
+		} else {
+			l = m
+		}
+	}
+	res[1] = r
+	return res
 }
